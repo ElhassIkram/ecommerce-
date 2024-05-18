3131b6f851577d30f135dc31d74b5d7efb7f6dab
nadahilal <nadahilalemais@gmail.com>
2024-05-19 00:13:32 +0100

    first

---
+++ app/Models/commandes.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class commandes extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'date',
+        'time',
+        'regle',
+        'mode_reglements_id',
+        'etat_id',
+        'user_id',
+    ];
+
+}
